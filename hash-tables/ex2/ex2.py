d3d80a6f9baab33a3e24988e670d700139758d88
alasalle <angela.lasalle91@gmail.com>
2019-08-03 02:00:51 -0400

    ex2 complete and passing tests MVP complete

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -19,5 +17,15 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    key = "NONE"
+
+    for i in range(length):
+        current_ticket = hash_table_retrieve(hashtable, key)
+        route[i] = current_ticket
+        key = current_ticket
 
     return route
